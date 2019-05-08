2ce48cda85fe7f5c9c7cd99af9831b155e368f5b
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-05-07 21:10:53 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import $ from 'jquery'
+import 'foundation-sites'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//=====Container
+
+//====Context
+
+const Err = () => {
+  return (<>
+    <h2 className='headerSpace'>404 Missing Page.</h2>
+  </>)
+}
+// WHEN USING FOUNDATION USE THIS JQUERY SELECTOR FOR ANY JAVASCRIPT FEATURES PULLED FROM FOUNDATION LOOK A THE EXAMPLE BELOW!
+class App extends Component {
+  componentDidMount=()=>{
+    $(document).foundation();
+  }
+  render() {
+   return( <>
+
+<div class="row">
+  <div class="columns">
+    <h2>Dropdown Pane</h2>
+    <p>Dropdown panes are little happy sprites which can be revealed on click or hover.</p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="columns">
+    <button class="button" type="button" data-toggle="example-dropdown">Toggle Dropdown</button>
+    <div class="dropdown-pane" id="example-dropdown" data-dropdown>
+      Just some junk that needs to be said. Or not. Your choice.
     </div>
-  );
+  </div>
+</div>
+    </>);
+  }
 }
 
 export default App;
