dc0f08a80c9393368f6eaf608a7f447e9868d5b9
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-05-18 23:04:23 -0400

    Homepage Init, footer init, brand added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import $ from 'jquery'
-import 'foundation-sites'
 
 //=====Container
-
+import Navbar from './components/Logo'
+import Home from './containers/Landing'
+import Footer from './components/Footer'
 //====Context
 
 const Err = () => {
@@ -12,29 +12,18 @@ const Err = () => {
     <h2 className='headerSpace'>404 Missing Page.</h2>
   </>)
 }
-// WHEN USING FOUNDATION USE THIS JQUERY SELECTOR FOR ANY JAVASCRIPT FEATURES PULLED FROM FOUNDATION LOOK A THE EXAMPLE BELOW!
 class App extends Component {
-  componentDidMount=()=>{
-    $(document).foundation();
-  }
+  
   render() {
    return( <>
-
-<div class="row">
-  <div class="columns">
-    <h2>Dropdown Pane</h2>
-    <p>Dropdown panes are little happy sprites which can be revealed on click or hover.</p>
-  </div>
-</div>
-
-<div class="row">
-  <div class="columns">
-    <button class="button" type="button" data-toggle="example-dropdown">Toggle Dropdown</button>
-    <div class="dropdown-pane" id="example-dropdown" data-dropdown>
-      Just some junk that needs to be said. Or not. Your choice.
-    </div>
-  </div>
-</div>
+ <HashRouter>
+          <Route path='/' component={Navbar} />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route component={Err} />
+          </Switch>
+          <Route path='/' component={Footer} />
+      </HashRouter>
     </>);
   }
 }
---
+++ src/containers/Offline/Offline.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import ExampleRecipe from '../../components/Offline/ExampleRecipe'
+import JoinToday from '../../components/Offline/JoinToday'
+import './offline.css'
+
+class Offline extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipes: [1, 2, 3, 4, 5, 6, 7]
+    }
+  }
+
+  Recipes = () => {
+    return this.state.recipes.map((e, i) => {
+      return (
+        <div className='foodContainer' key={i}>
+          <ExampleRecipe />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (<>
+      <div className='row banner'>
+        <div className='col offset-s7 offset-l7 s5 l5 bannerContainer'>
+          <div className='bannerText'>
+            <h4><i>Search. Chat. Cook.</i></h4>
+            <span className='flow-text'>Find the perfect recipe from a wide selection fit for </span>
+            <span className='flow-text flavorText'>You</span>
+            <span className='flow-text'>r needs. </span>
+            <span className='flow-text flavorText'>You</span>
+            <span className='flow-text'>r Ingredients. </span>
+            <span className='flow-text flavorText'>You</span>
+            <span className='flow-text'>r Styles. </span>
+            <span className='flow-text flavorText'>You</span>
+            <span className='flow-text'>r Favorites.</span>
+            <h5 style={{ textAlign: 'right' }}> Simply </h5>
+            <h5 className='flavorText' style={{ textAlign: 'right' }}>You.</h5>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row' style={{ textAlign: 'center' }}>
+          <div className='col s12 l4 noPadding' >
+            <h3>Search</h3>
+            <div className='orange lighten-4 valign-wrapper'>
+              <p className='flow-text' style={{ padding: '10px', height: '200px' }}>
+                Search recipes with Ingredients you have and cook your favorites!
+            </p>
+            </div>
+          </div>
+          <div className='col s12 l4 noPadding'>
+            <h3>Chat</h3>
+            <div className='orange lighten-3 valign-wrapper'>
+              <p className='flow-text' style={{ padding: '10px', height: '200px' }}>
+                Voice assisted controls to navigate the recipe. Never lift a finger again.
+            </p>
+            </div>
+          </div>
+          <div className='col s12 l4 noPadding' >
+            <h3>Cook</h3>
+            <div className='orange lighten-2 valign-wrapper'>
+              <p className='flow-text' style={{ padding: '10px', height: '200px' }}>
+                No jumbling up steps, minimalistic display shows what you need exactly when you need them.
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <h1>GIF HERE OF APP IN USE</h1>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <div className='foodSlider' >
+        <this.Recipes />
+      </div>
+      <div className='container' style={{ marginTop: '40px', backgroundColor:'#F1F1F1'}}>
+        <div className='row' >
+          <div className='col s12 l6'>
+            <h3 style={{textAlign:'center'}}>Join Today!</h3>
+            <img src='https://cdn.shopify.com/s/files/1/0543/1257/products/savor-recipe_buy-square_variants_01.jpg?v=1550781962' 
+            alt='Join'
+            style={{width:'100%'}}/>
+          </div>
+          <JoinToday />
+        </div></div>
+    </>)
+  }
+}
+
+export default Offline;
