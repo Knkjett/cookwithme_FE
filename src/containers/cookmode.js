5b4e81992b61d50b3a096bbd39b53ebaaf3fcc5a
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-06-01 15:28:12 -0400

    adding max height for cookmode steps + changing carousel style on user profile

--- src/containers/cookmode.js
+++ src/containers/cookmode.js
@@ -5,7 +5,7 @@ import Axios from 'axios';
 class Cookmode extends Component {
 
   state={
-    steps: ['1. prep stuff. This is also where I now I will ramble so that this is much longer to test how the styling may change when I make this super long. So long, in fact, that this div will get huge. Seriously, I want to test this bad boy out. I want to make sure the buttons stay right above the footer on this page. Fingers crossed.', '2. cook stuff', '3. cool food', '4. serve food', '5. Rinse and Repeat'], // for testing purposes
+    steps: ['1. prep stuff. This is also where I now I will ramble so that this is much longer to test how the styling may change when I make this super long. So long, in fact, that this div will get huge. Seriously, I want to test this bad boy out. I want to make sure the buttons stay right above the footer on this page. Fingers crossed. I;M TYPING MORE STUFF NOW TO MAKE THIS EVEN BIGGER THAN IT WAS. HHHKBB;G;UG;U OUUO;;;OUG;OUG ;OU;OUGO ;OUG;OGU O LIG;IG;IG ;UG;G;GU ;UGUGH;', '2. cook stuff', '3. cool food', '4. serve food', '5. Rinse and Repeat'], // for testing purposes
     currentStepIndex:0,
     stepsLength:0,
   }
@@ -14,7 +14,7 @@ class Cookmode extends Component {
     let elems = document.querySelectorAll('.collapsible');
     Materialize.Collapsible.init(elems, { accordion: true });
     // const { ingredients, steps } = this.props.location.cook  //object with two arrays
-    const { steps, currentStepIndex, stepsLength} = this.state;
+    const { steps } = this.state;
     this.setState({stepsLength:steps.length-1})
   }
 
@@ -37,20 +37,20 @@ class Cookmode extends Component {
     const { steps, currentStepIndex } = this.state;
     return <>
       <div className='cookBG'>
-        <div class="row container">
-          <div class="col s12 m8">
-            <div class="card-panel white opacitywebmobile" style={{ maxHeight: '610px', overflow: 'scroll' }}>
-              <span class="black-text fontwebmobile" style={{ fontFamily: 'Lucida Sans, Lucida Sans Regular, Lucida Grande, Lucida Sans Unicode, Geneva, Verdana, sans-serif', opacity: 1 }}>
+        <div className="row container">
+          <div className="col s12 m8">
+            <div className="card-panel white opacitywebmobile" style={{ maxHeight: '500px', overflow: 'scroll' }}>
+              <span className="black-text fontwebmobile" style={{ fontFamily: 'Lucida Sans, Lucida Sans Regular, Lucida Grande, Lucida Sans Unicode, Geneva, Verdana, sans-serif', opacity: 1 }}>
                 {steps[currentStepIndex]}
             </span>
             </div>
           </div>
-          <div class="col s12 m4">
-            <ul class="collapsible" data-collapsible="accordion">
+          <div className="col s12 m4">
+            <ul className="collapsible" data-collapsible="accordion">
               <li>
-                <div class="collapsible-header">
-                  <i class="material-icons">dehaze</i>Ingredients</div>
-                <div class="collapsible-body">
+                <div className="collapsible-header">
+                  <i className="material-icons">dehaze</i>Ingredients</div>
+                <div className="collapsible-body">
                   <ul>
                     <li>2 medium sweet potatoes</li>
                     <li>1 1/2 to 2 tablespoons extra-virgin olive oil</li>
@@ -62,15 +62,15 @@ class Cookmode extends Component {
             </ul>
           </div>
         </div>
-        <div class='row container button-container'>
-          <div class='col s4 m4' style={{ paddingLeft: 'auto' }}>
-            <a class="btn-floating btn-large waves-effect waves-light red" onClick={this.HandleBackClick}><i class="material-icons">arrow_back</i></a>
+        <div className='row container button-container'>
+          <div className='col s4 m4' style={{ paddingLeft: 'auto' }}>
+            <a className="btn-floating btn-large waves-effect waves-light red" onClick={this.HandleBackClick}><i className="material-icons">arrow_back</i></a>
           </div>
-          <div class='col s4 m4'>
-            <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>
+          <div className='col s4 m4'>
+            <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">play_arrow</i></a>
           </div>
-          <div class='col s4 m4'>
-            <a class="btn-floating btn-large waves-effect waves-light red" onClick={this.HandleForwardClick}><i class="material-icons">arrow_forward</i></a>
+          <div className='col s4 m4'>
+            <a className="btn-floating btn-large waves-effect waves-light red" onClick={this.HandleForwardClick}><i className="material-icons">arrow_forward</i></a>
           </div>
         </div>
       </div>
