6e0e8dd2d5c78f186de52aa855262a0bea6b3fce
Tarek <bayridge4th@gmail.com>
2019-06-02 22:05:25 -0400

    made toggable favorites button, added more default recipes, and made search bar

--- src/containers/SignInHome.js
+++ src/containers/SignInHome.js
@@ -2,52 +2,115 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './SignInHome.css';
 import axios from 'axios';
+import {defaultRecipes} from '../services/services'
+import AuthContext from '../contexts/auth'
+import M from 'materialize-css';
 
 class SignInHome extends Component{
     state = {
-        recipes:[]
+        recipes:[],
+        buttonActive:'disabled',
+        favs:[],
     }
     componentDidMount(){
-        axios.get('https://www.food2fork.com/api/search?key=ee476d8f542bef2e97d8bf30c7f3c0ca')
-        .then(res=>{
-            //console.log(res.data)
-            let recipes_arr = res.data.recipes.filter(e=>{
-                return e.publisher ==='Closet Cooking' || e.publisher === "The Pioneer Woman" || e.publisher === 'All Recipes'
-            })
-            recipes_arr = recipes_arr.slice(12)
-            this.setState({recipes:recipes_arr},()=>console.log(this.state))
+        defaultRecipes().then(recipes=>{
+            let favs = Array(recipes.length).fill(0,0)
+            this.setState({recipes:recipes,favs:favs})
         })
+        var elems = document.querySelectorAll('.fixed-action-btn');
+        M.FloatingActionButton.init(elems, {
+            direction: 'left',
+            hoverEnabled: false
+          });
     }
-    render(){
-        return <>
-        <div className='signInHomePageBG'>
-            <div className="container webContainer " >
-            <div className="row" style={{marginBottom:'0px'}}>
-                {this.state.recipes.map((obj,i)=>{
-                    console.log(obj)
-                    return <Link to={{ 
-                    pathname: `/recipepage/${obj.title}`, 
-                    state: { url: obj.source_url, publisher: obj.publisher_url, source_img: obj.image_url } 
-                  }}>
-                  <div className="col s12 m4">
-                        <div className="card">
-                            <div className="card-image">
-                                <img style={{height:'281.17px'}} src={obj.image_url} />
 
-                                <a className="btn-floating halfway-fab waves-effect waves-light red"><i
-                                        className="material-icons">add</i></a>
-                            </div>
-                            <div className="card-content" style={{height:'100px'}}>
-                                <span className="card-title" style={{fontSize:'18px'}}>{obj.title}</span>
+    toggleFav(i,user){
+        const {favs} = this.state
+        favs[i] = favs[i] === 0 ? 1 : 0
+        this.setState({favs:favs})
+    }
+    render(){ return (
+            <><AuthContext.Consumer>
+                {
+                    user => {
+                        return (<div className='signInHomePageBG'>
+                        <div className=" webContainer " >
+                        <div className="row" style={{marginBottom:'0px'}}>
+                            {this.state.recipes.map((obj,i)=>{
+                                let classStr = null
+                                const font = {fontFamily:'Futura',fontSize: '20px',fontWeight:300}
+                                if(obj.title.length > 35) font.fontSize = '20px'
+                                
+                                if(this.state.favs[i] ===  0) classStr = 'btn-floating disabled halfway-fab waves-light red'
+                                if(this.state.favs[i] === 1) classStr = 'btn-floating halfway-fab waves-light red'
+                                //console.log(classStr)
+                                return <div className="col s12 m3">
+                                    <div className="card">
+                                        <div className="card-image">
+                                        
+                                            <img onClick={()=>this.toggleFav(i,user)} style={{height:'236.17px'}} src={obj.image_url} />
+                                            
+                                            <a className={classStr}><i 
+                                                    className="material-icons">add</i></a>
+                                        </div>
+                                        <div className="card-content" style={{height:'100px',textAlign:'center'}}>
+                                        <Link to={{ 
+                                            pathname: `/recipepage/${obj.title}`, 
+                                            state: { url: obj.source_url, publisher: obj.publisher_url, source_img: obj.image_url } 
+                                        }}>
+                                            <span className="card-title" style={font}>{obj.title}</span>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div> 
+                                
+                            })}
                             </div>
                         </div>
-                    </div> 
-                    </Link>
-                })}
-                </div>
-            </div>
+                    </div>)
+                    }
+                }
+            </AuthContext.Consumer>
+            <div class="fixed-action-btn click-to-toggle">
+            <a class="btn-floating btn-large red">
+              <i class="large material-icons">mode_edit</i>
+            </a>
+            <ul>
+              <li>
+              <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required style={{background:'bisque',width:'300px',height:'50px'}}/>
+          
+          
         </div>
-        </>
+      </form>
+    </div>
+                  {/* <div class="input-field">
+          <input id="search" type="search" required style={{background:'bisque',width:'400px'}} />
+          <label class="label-icon" for="search"></label>
+          
+        </div> */}
+        </li>
+              
+            </ul>
+          </div>
+          
+          
+          <nav>
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required/>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+          </>
+          
+    )
     }
 }
 
