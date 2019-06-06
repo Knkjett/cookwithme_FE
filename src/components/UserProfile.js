42471e562f3b386d19df6b00241a7e82a0590159
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-06-05 20:24:16 -0400

    adding Auth Consumer

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Materialize from 'materialize-css/dist/js/materialize.min.js';
 import '../components/UserProfile.css'
+import AuthContext from '../contexts/auth';
 
 export default class UserProfile extends Component {
   constructor(props) {
@@ -8,15 +9,16 @@ export default class UserProfile extends Component {
 
     this.state = {
       user: null,
-      input: '',
-      groceryList: ['Eggs', 'Milk', 'Butter', 'Sugar', 'Salt', 'Flour', 'Chicken', 'Beef', 'Pork Chops', 'BBQ Sauce', 'Broccoli', 'Green Beans', 'Black Beans'],
       favorites: [],
       yourRecipes: [],
+      // recentlyViewed: [], --> Need Local Storage?
     }
   }
 
   componentDidMount = () => {
+    // Scroll to top of page upon loading
     window.scrollTo(0, 0);
+
     // Carousel Initialization
     let elems = document.querySelectorAll('.carousel');
     Materialize.Carousel.init(elems, { duration: '200' });
@@ -25,195 +27,163 @@ export default class UserProfile extends Component {
     let elems2 = document.querySelectorAll('.collapsible');
     Materialize.Collapsible.init(elems2, { accordion: true });
 
-    // Parallax Initialization
-    // var elems3 = document.querySelectorAll('.parallax');
-    // Materialize.Parallax.init(elems3, {responsiveThreshold: 0});
   }
 
-  ListItem = () => {
-    if (!this.state.groceryList) return <></>
-    else {
-      return this.state.groceryList.map((e, i) => {
-        return (
-          <p key={i}>
-            <label>
-              <input type="checkbox" />
-              <span>{e}</span>
-            </label>
-          </p>)
-      })
-    }
-  }
 
-  // AddItem = (e) => {
-  //   const {input} = this.state;
-  //   input = e.target.value;
-  //   this.setState({input})
-  // }
+
 
   render() {
     return (<>
-      <div className='background'>
-        {/* -------------------------- Styling for Mobile App ----------------------------*/}
-        <div className="show-on-small hide-on-med-and-up" style={{ backgroundColor: 'orange' }}>
-          <h3 style={{ marginTop: '0px', textAlign: 'center', paddingTop: '150px' }}>Welcome back, User!</h3>
-          <ul className="collapsible" style={{ marginTop: '125px' }}>
-            <li>
-              <div className="collapsible-header">Recently Viewed</div>
-              <div className="collapsible-body">
-                <div className="row userSlider" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
-                  {/* RECENTLY VIEWED CARD */}
-                  <div className="col s6 card small card-container">
-                    <div className="card-image">
-                      <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
-                      <span className="card-title">Recipe Name</span>
-                    </div>
-                    <div className="card-content">
-                      <p>Quick blurb about this recipe, maybe.</p>
-                    </div>
-                    <div className="card-action">
-                      <a href="/#">This is a link</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="collapsible-header">Favorites</div>
-              <div className="collapsible-body">
-                <div className="row userSlider" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
-                  {/* FAVORITES CARD */}
-                  <div className="col s6 card small card-container">
-                    <div className="card-image">
-                      <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
-                      <span className="card-title">Recipe Name</span>
-                    </div>
-                    <div className="card-content">
-                      <p>Quick blurb about this recipe, maybe.</p>
-                    </div>
-                    <div className="card-action">
-                      <a href="/#">This is a link</a>
+      <AuthContext.Consumer>
+        <div className='background'>
+          {/* -------------------------- Styling for Mobile App ----------------------------*/}
+          <div className="show-on-small hide-on-med-and-up" style={{ backgroundColor: 'orange' }}>
+            <h3 style={{ marginTop: '0px', textAlign: 'center', paddingTop: '150px' }}>Welcome back, User!</h3>
+            <ul className="collapsible" style={{ marginTop: '125px' }}>
+              <li>
+                <div className="collapsible-header">Favorites</div>
+                <div className="collapsible-body">
+                  <div className="row userSlider" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
+                    {/* FAVORITES CARD */}
+                    <div className="col s6 card small card-container">
+                      <div className="card-image">
+                        <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
+                        <span className="card-title">Recipe Name</span>
+                      </div>
+                      <div className="card-content">
+                        <p>Quick blurb about this recipe, maybe.</p>
+                      </div>
+                      <div className="card-action">
+                        <a href="/#">This is a link</a>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>
-            <li>
-              <div className="collapsible-header">Your Recipes</div>
-              <div className="collapsible-body">
-                <div className="row" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
-                  {/* USER CREATED RECIPE CARD */}
-                  <div className="col s6 card small card-container">
-                    <div className="card-image">
-                      <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
-                      <span className="card-title">Card Title</span>
-                    </div>
-                    <div className="card-content">
-                      <p>Quick blurb about this recipe, maybe.</p>
-                    </div>
-                    <div className="card-action">
-                      <a href="/#">This is a link</a>
+              </li>
+              <li>
+                <div className="collapsible-header">Your Recipes</div>
+                <div className="collapsible-body">
+                  <div className="row" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
+                    {/* USER CREATED RECIPE CARD */}
+                    <div className="col s6 card small card-container">
+                      <div className="card-image">
+                        <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
+                        <span className="card-title">Card Title</span>
+                      </div>
+                      <div className="card-content">
+                        <p>Quick blurb about this recipe, maybe.</p>
+                      </div>
+                      <div className="card-action">
+                        <a href="/#">This is a link</a>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          </ul>
-        </div>
+              </li>
+              <li>
+                <div className="collapsible-header">Recently Viewed</div>
+                <div className="collapsible-body">
+                  <div className="row userSlider" style={{ display: 'inline-flex', width: '100vw', overflow: 'scroll' }}>
+                    {/* RECENTLY VIEWED CARD */}
+                    <div className="col s6 card small card-container">
+                      <div className="card-image">
+                        <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
+                        <span className="card-title">Recipe Name</span>
+                      </div>
+                      <div className="card-content">
+                        <p>Quick blurb about this recipe, maybe.</p>
+                      </div>
+                      <div className="card-action">
+                        <a href="/#">This is a link</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
 
-        {/* -------------------------- Styling for Web App ----------------------------*/}
-        <div className="show-on-large hide-on-small-only">
-          <h1 style={{ marginTop: '0px', textAlign: 'center', paddingTop: '50px' }}>Welcome back, User!</h1>
-          {/* FAVORTITES */}
-          <div className="row">
-            <h5>Your Favorites:</h5>
-            <div className="row userSlider" style={{ display: 'inline-flex', width: '80%', overflow: 'scroll' }}>
-              {/* FAVORITES CARD */}
-              <div className="col s6 card small card-container">
-                <div className="card-image">
-                  <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
-                  <span className="card-title">Recipe Name</span>
+          {/* -------------------------- Styling for Web App ----------------------------*/}
+          <div className="show-on-large hide-on-small-only">
+            <h1 style={{ marginTop: '0px', textAlign: 'center', paddingTop: '50px' }}>Welcome back, User!</h1>
+            {/* FAVORTITES */}
+            <div className="row">
+              <h5>Your Favorites:</h5>
+              <div className="row userSlider" style={{ display: 'inline-flex', width: '80%', overflow: 'scroll' }}>
+                {/* FAVORITES CARD */}
+                <div className="col s6 card small card-container">
+                  <div className="card-image">
+                    <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
+                    <span className="card-title">Recipe Name</span>
+                  </div>
+                  <div className="card-content">
+                    <p>Quick blurb about this recipe, maybe.</p>
+                  </div>
                 </div>
-                <div className="card-content">
-                  <p>Quick blurb about this recipe, maybe.</p>
+              </div>
+              {/* YOUR RECIPES  */}
+              <h5>Your Recipes:</h5>
+              <div className="row" style={{ display: 'inline-flex', width: '80%', overflow: 'scroll' }}>
+                {/* USER CREATED REICPES CARD */}
+                <div className="col s6 card small card-container">
+                  <div className="card-image">
+                    <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
+                    <span className="card-title">Card Title</span>
+                  </div>
+                  <div className="card-content">
+                    <p>Quick blurb about this recipe, maybe.</p>
+                  </div>
                 </div>
               </div>
             </div>
-            {/* GROCERY LIST CARD ---> removing this for now */}
-            {/* <div className="card grocery-container">
-              <div className="card-action white-text blue-grey">
-                <span className="card-title">Grocery List</span>
-              </div>
-              <div className="card-content grocery-list">
-                <this.ListItem />
-              </div>
-              <div className="card-action">
-                <a className="btn-floating btn-small waves-effect waves-light red" href='/'><i className="material-icons">add</i></a>
-                <span>Add To List</span>
-              </div>
-            </div> */}
-            {/* YOUR RECIPES  */}
-            <h5>Your Recipes:</h5>
-            <div className="row" style={{ display: 'inline-flex', width: '80%', overflow: 'scroll' }}>
-              {/* USER CREATED REICPES CARD */}
-              <div className="col s6 card small card-container">
-                <div className="card-image">
-                  <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg" alt='food pic' />
-                  <span className="card-title">Card Title</span>
-                </div>
-                <div className="card-content">
-                  <p>Quick blurb about this recipe, maybe.</p>
-                </div>
-              </div>
+            <div className='createRecipe-container'>
+              <a href='/#/create' className="waves-effect waves-light btn-large">Create Recipe</a>
             </div>
-          </div>
-          <div className='createRecipe-container'>
-            <a href='/#/create' className="waves-effect waves-light btn-large">Create Recipe</a>
-          </div>
-          {/* RECENTLY VIEWED CAROUSEL */}
-          <h5>Recently Viewed:</h5>
-          <div class="carousel">
-            <div class="carousel-item">
-              <div class="card sticky-action">
-                <div class="card-image ">
-                  <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg?h=350" alt='food-pic' />
-                  <span class="card-title">Recipe Name</span>
-                </div>
-                <div class="card-content">
-                  <p>This is a content.</p>
+            {/* RECENTLY VIEWED CAROUSEL */}
+            <h5>Recently Viewed:</h5>
+            <div class="carousel">
+              <div class="carousel-item">
+                <div class="card sticky-action">
+                  <div class="card-image ">
+                    <img src="http://baliindiancuisine.com/wp-content/uploads/2014/12/Indian-fast-food-recipes.jpg?h=350" alt='food-pic' />
+                    <span class="card-title">Recipe Name</span>
+                  </div>
+                  <div class="card-content">
+                    <p>This is a content.</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="carousel-item">
-              <div class="card">
-                <div class="card-image">
-                  <img src="https://images.pexels.com/photos/160933/girl-rabbit-friendship-love-160933.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
-                  <span class="card-title">This is a Title</span>
+              <div class="carousel-item">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="https://images.pexels.com/photos/160933/girl-rabbit-friendship-love-160933.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
+                    <span class="card-title">This is a Title</span>
+                  </div>
+                  <span class="card-content">This is a content</span>
                 </div>
-                <span class="card-content">This is a content</span>
               </div>
-            </div>
-            <div class="carousel-item">
-              <div class="card">
-                <div class="card-image">
-                  <img src="https://images.pexels.com/photos/160699/girl-dandelion-yellow-flowers-160699.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
-                  <span class="card-title">This is a Title</span>
+              <div class="carousel-item">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="https://images.pexels.com/photos/160699/girl-dandelion-yellow-flowers-160699.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
+                    <span class="card-title">This is a Title</span>
+                  </div>
+                  <span class="card-content">This is a content</span>
                 </div>
-                <span class="card-content">This is a content</span>
               </div>
-            </div>
-            <div class="carousel-item">
-              <div class="card">
-                <div class="card-image">
-                  <img src="https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
-                  <span class="card-title">This is a Title</span>
+              <div class="carousel-item">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?h=350&auto=compress&cs=tinysrgb" alt='food-pic' />
+                    <span class="card-title">This is a Title</span>
+                  </div>
+                  <span class="card-content">This is a content</span>
                 </div>
-                <span class="card-content">This is a content</span>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </AuthContext.Consumer>
     </>
     )
   }
